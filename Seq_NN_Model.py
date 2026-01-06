7c45467edb9a5e645556e474d174ba7772443bbd
ahmad777x86 <087m.ahmad@gmail.com>
2026-01-06 14:33:12 +0500

    Made Sequential model class

--- Seq_NN_Model.py
+++ Seq_NN_Model.py
@@ -2,8 +2,14 @@ class Sequential_Model:
     def __init__(self, Layers):
         self.Layers = Layers
     
-    def forward(self):
-        pass
+    def forward(self, X):
+        self.signal = X
+        for layer in self.Layers:
+            self.signal = layer.forward(self.signal)
+        print("Prediction: ", self.Layers[-1].output)
 
-    def backward(self):
-        pass
+    def backward(self, loss_grad):
+        self.grad = loss_grad
+        for layer in self.Layers[::-1]:
+            self.grad = layer.backward(self.grad)
+        print("Gradients Calculated!")
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ReLU_Layer import ReLU_Layer
 from Sigmoid_Layer import Sigmoid_Layer
 from Dense_Layer import Dense_Layer
 from BCE_Loss import Binary_Cross_Entropy_Loss
+from Seq_NN_Model import Sequential_Model
 
 X = np.array([[1,1],[0,0],[0,1],[1,0]])
 y = np.array([[1],[1],[0],[0]])
@@ -16,44 +17,33 @@ layer2 = ReLU_Layer()
 layer3 = Dense_Layer(4,1)
 layer4 = Sigmoid_Layer()
 Layers = [layer1, layer2, layer3, layer4]
+model = Sequential_Model(Layers)
 
 # Loss
 loss = Binary_Cross_Entropy_Loss()
 
 # forward pass
-def predict():
-    signal = X
-    for layer in Layers:
-        signal = layer.forward(signal)
-    print("Prediction: ", Layers[-1].output)
-
-
+model.forward(X)
 
 # backward pass
 Loss = []
 for _ in range(1000):
-    predict()
+
+    model.forward(X)
     Loss.append(loss.forward(y,layer4.output)[0])
     print(f"Loss: {Loss[_]}")
+
     loss_gradient = loss.backward(y,layer4.output)
     print("Loss grad shape: ", loss_gradient.shape)
-    grad_4 = layer4.backward(loss_gradient)
-    grad_3 = layer3.backward(grad_4)
-    print("Input Gradient 3: ", grad_3)
-    print("Weight Gradient 3: ",layer3.w_gradient)
+
+    model.backward(loss_gradient)
+
     layer3.weights -= 0.1 * layer3.w_gradient
     layer3.bias -= 0.1 * layer3.b_gradient
-
-    grad_2 = layer2.backward(grad_3)
-    print("Gradient 2: ", grad_2)
-
-    grad_1 = layer1.backward(grad_2)
-    print("Gradient 1: ", grad_1)
-    print("Weight Gradient 1: ",layer1.w_gradient)
     layer1.weights -= 0.1 * layer1.w_gradient
     layer1.bias -= 0.1 * layer1.b_gradient
 
-predict()
+model.forward(X)
 
 plt.plot(range(1000), Loss, 'r-')
 plt.title("Binary Cross Entropy Loss over Epochs")
